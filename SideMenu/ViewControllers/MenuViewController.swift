401b8453a09d5dcfbabce921f9dd41b3fa708b81
Shuaiqing Luo <luoshuaiqing>
2023-12-16 17:43:47 -0800

    update

--- SideMenu/ViewControllers/MenuViewController.swift
+++ SideMenu/ViewControllers/MenuViewController.swift
@@ -15,24 +15,47 @@ class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDe
         case appRating = "App Rating"
         case shareApp = "Share App"
         case settings = "Settings"
+        
+        var imageName: String {
+            switch self {
+            case .home:
+                return "house"
+            case .info:
+                return "airplane"
+            case .appRating:
+                return "star"
+            case .shareApp:
+                return "message"
+            case .settings:
+                return "gear"
+            }
+        }
     }
+    
+    let greyColor = UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
     
     private let tableView: UITableView = {
         let table = UITableView()
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.backgroundColor = nil
         return table
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+        tableView.delegate = self
 
-        view.backgroundColor = UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
+        view.backgroundColor = greyColor
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        tableView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.bounds.size.width, height: view.bounds.size.height)
-    }
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        tableView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.bounds.size.width, height: view.bounds.size.height)
+//    }
     
     // Table
     
@@ -42,7 +65,12 @@ class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.textColor = .white
         cell.textLabel?.text = MenuOptions.allCases[indexPath.row].rawValue
+        cell.backgroundColor = greyColor
+        cell.contentView.backgroundColor = greyColor
+        cell.imageView?.image = UIImage(systemName: MenuOptions.allCases[indexPath.row].imageName)
+        cell.imageView?.tintColor = .white
         return cell
     }
     
